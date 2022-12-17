6bb41324fc86ab1863e48ce90f1ff087265b7500
harsh6975 <harshu1234ashv@gmail.com>
2022-12-17 19:53:41 +0530

    Home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./component/Header/Header";
+import Home from "./component/Home/Home";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
     </div>
   );
 }
---
+++ src/component/Home/Home.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import style from "./Home.module.css";
+import MovieCard from "../MovieCard/MovieCard";
+
+function Home() {
+  const [moviesList, setMoviesList] = useState([]);
+
+  const getMovies = async () => {
+    const movies = await axios.get(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+    );
+    setMoviesList(movies.data.results);
+    console.log(movies);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <div className={style.home}>
+      <h3>Most Recent Movies</h3>
+      <div className={style.movie}>
+        {moviesList.map((movie) => {
+          return <MovieCard movie={movie} />
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
